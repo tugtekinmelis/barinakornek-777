83d452485f76ac68ffed1c955521989132185c68
tugtekinmelis <145451246+tugtekinmelis@users.noreply.github.com>
2024-05-21 09:55:25 +0300

    Add files via upload

---
+++ barinak-777/ListeleForm.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace barinak_777
+{
+    partial class ListeleForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.dgwHayvanlar = new System.Windows.Forms.DataGridView();
+            this.pbResim = new System.Windows.Forms.PictureBox();
+            this.cbEngel = new System.Windows.Forms.CheckBox();
+            this.cmbTur = new System.Windows.Forms.ComboBox();
+            this.txtYas = new System.Windows.Forms.TextBox();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwHayvanlar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbResim)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(445, 121);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(169, 83);
+            this.btnSil.TabIndex = 35;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Location = new System.Drawing.Point(445, 16);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(169, 83);
+            this.btnGuncelle.TabIndex = 34;
+            this.btnGuncelle.Text = "Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // dgwHayvanlar
+            // 
+            this.dgwHayvanlar.AllowUserToAddRows = false;
+            this.dgwHayvanlar.AllowUserToDeleteRows = false;
+            this.dgwHayvanlar.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgwHayvanlar.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgwHayvanlar.Location = new System.Drawing.Point(388, 258);
+            this.dgwHayvanlar.Name = "dgwHayvanlar";
+            this.dgwHayvanlar.ReadOnly = true;
+            this.dgwHayvanlar.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgwHayvanlar.Size = new System.Drawing.Size(519, 268);
+            this.dgwHayvanlar.TabIndex = 33;
+            this.dgwHayvanlar.SelectionChanged += new System.EventHandler(this.dgwHayvanlar_SelectionChanged);
+            // 
+            // pbResim
+            // 
+            this.pbResim.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pbResim.Location = new System.Drawing.Point(140, 156);
+            this.pbResim.Name = "pbResim";
+            this.pbResim.Size = new System.Drawing.Size(209, 206);
+            this.pbResim.TabIndex = 32;
+            this.pbResim.TabStop = false;
+            this.pbResim.Click += new System.EventHandler(this.pbResim_Click);
+            // 
+            // cbEngel
+            // 
+            this.cbEngel.AutoSize = true;
+            this.cbEngel.Location = new System.Drawing.Point(327, 120);
+            this.cbEngel.Name = "cbEngel";
+            this.cbEngel.Size = new System.Drawing.Size(15, 14);
+            this.cbEngel.TabIndex = 31;
+            this.cbEngel.UseVisualStyleBackColor = true;
+            // 
+            // cmbTur
+            // 
+            this.cmbTur.FormattingEnabled = true;
+            this.cmbTur.Location = new System.Drawing.Point(158, 86);
+            this.cmbTur.Name = "cmbTur";
+            this.cmbTur.Size = new System.Drawing.Size(185, 21);
+            this.cmbTur.TabIndex = 30;
+            // 
+            // txtYas
+            // 
+            this.txtYas.Location = new System.Drawing.Point(158, 48);
+            this.txtYas.Name = "txtYas";
+            this.txtYas.Size = new System.Drawing.Size(185, 20);
+            this.txtYas.TabIndex = 29;
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(158, 8);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(185, 20);
+            this.txtAd.TabIndex = 28;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(92, 156);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 27;
+            this.label4.Text = "Resim :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(92, 121);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 13);
+            this.label5.TabIndex = 26;
+            this.label5.Text = "Engel Durumu :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(92, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 25;
+            this.label3.Text = "Türü :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(92, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "Yaşı :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(92, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "Adı :";
+            // 
+            // ListeleForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(933, 622);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.dgwHayvanlar);
+            this.Controls.Add(this.pbResim);
+            this.Controls.Add(this.cbEngel);
+            this.Controls.Add(this.cmbTur);
+            this.Controls.Add(this.txtYas);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "ListeleForm";
+            this.Text = "ListeleForm";
+            this.Load += new System.EventHandler(this.ListeleForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgwHayvanlar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbResim)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.DataGridView dgwHayvanlar;
+        private System.Windows.Forms.PictureBox pbResim;
+        private System.Windows.Forms.CheckBox cbEngel;
+        private System.Windows.Forms.ComboBox cmbTur;
+        private System.Windows.Forms.TextBox txtYas;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ barinak-777/YeniEkleForm.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace barinak_777
+{
+    partial class YeniEkleForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.pbResim = new System.Windows.Forms.PictureBox();
+            this.cmbTur = new System.Windows.Forms.ComboBox();
+            this.txtYas = new System.Windows.Forms.TextBox();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbEngel = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbResim)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(174, 427);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(209, 66);
+            this.btnEkle.TabIndex = 20;
+            this.btnEkle.Text = "Kaydet";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // pbResim
+            // 
+            this.pbResim.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pbResim.Location = new System.Drawing.Point(174, 188);
+            this.pbResim.Name = "pbResim";
+            this.pbResim.Size = new System.Drawing.Size(209, 206);
+            this.pbResim.TabIndex = 19;
+            this.pbResim.TabStop = false;
+            this.pbResim.Click += new System.EventHandler(this.pbResim_Click);
+            // 
+            // cmbTur
+            // 
+            this.cmbTur.FormattingEnabled = true;
+            this.cmbTur.Location = new System.Drawing.Point(198, 118);
+            this.cmbTur.Name = "cmbTur";
+            this.cmbTur.Size = new System.Drawing.Size(185, 21);
+            this.cmbTur.TabIndex = 18;
+            // 
+            // txtYas
+            // 
+            this.txtYas.Location = new System.Drawing.Point(198, 80);
+            this.txtYas.Name = "txtYas";
+            this.txtYas.Size = new System.Drawing.Size(185, 20);
+            this.txtYas.TabIndex = 17;
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(198, 40);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(185, 20);
+            this.txtAd.TabIndex = 16;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(132, 188);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Resim :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(132, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Türü :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(132, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Yaşı :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(132, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Adı :";
+            // 
+            // cbEngel
+            // 
+            this.cbEngel.AutoSize = true;
+            this.cbEngel.Location = new System.Drawing.Point(79, 155);
+            this.cbEngel.Name = "cbEngel";
+            this.cbEngel.Size = new System.Drawing.Size(93, 17);
+            this.cbEngel.TabIndex = 21;
+            this.cbEngel.Text = "engel durumu ";
+            this.cbEngel.UseVisualStyleBackColor = true;
+            // 
+            // YeniEkleForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 531);
+            this.Controls.Add(this.cbEngel);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.pbResim);
+            this.Controls.Add(this.cmbTur);
+            this.Controls.Add(this.txtYas);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "YeniEkleForm";
+            this.Text = "YeniEkleForm";
+            this.Load += new System.EventHandler(this.YeniEkleForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbResim)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.PictureBox pbResim;
+        private System.Windows.Forms.ComboBox cmbTur;
+        private System.Windows.Forms.TextBox txtYas;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox cbEngel;
+    }
+}
